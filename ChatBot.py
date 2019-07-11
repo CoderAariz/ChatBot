6b72f00fb9ab7dde0dfd750e5b1f98da8c45d392
CoderAariz <38472646+CoderAariz@users.noreply.github.com>
2019-07-11 23:15:40 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,6 +1,6 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-bot=ChatBot('Nova',read_only=True)
+bot=ChatBot('Nova',read_only=True)#you can change the read_only to false if you want your bot to learn from the chatting experience.
 trainer=ChatterBotCorpusTrainer(bot)
 trainer.train('C:/Users/Sharqua\Anaconda3\Lib\site-packages\chatterbot_corpus\data\english')
 while True:
